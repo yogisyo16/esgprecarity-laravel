9c1a94bd717f8a133afc97aeb9e27cbde1c76b22
yogis <yogisyo16@gmail.com>
2025-12-23 01:52:06 +0700

    Update: Change flag responsive

--- resources/views/pages/responden/responden-landing.blade.php
+++ resources/views/pages/responden/responden-landing.blade.php
@@ -7,18 +7,18 @@
 @push('language')
     <details class="dropdown">
         <summary class="btn btn-outline flex-wrap w-26 pb-10 md:w-auto md:pb-0 mr-6 md:mr-4">
-            {{ __('messages.language_title') }}: {{ __('messages.language') }} <img src="{{ __('messages.flag_languages') }}" alt="" class="w-12">
+            {{ __('messages.language_title') }}: {{ __('messages.language') }} <img src="{{ __('messages.flag_languages') }}" alt="" class="hidden md:block w-12">
         </summary>
-        <ul class="menu dropdown-content bg-base-100 rounded-box z-1 w-40 p-2 shadow-sm" data-theme="dark">
+        <ul class="menu dropdown-content bg-base-100 rounded-box z-1 w-30 md:w-40 p-2 shadow-sm" data-theme="dark">
             <li>
                 <a href="{{ route('set.language', 'en') }}">
-                    <img src="images/flag_england.png" alt="" class="w-4/5">
+                    <img src="images/flag_england.png" alt="" class="hidden md:block w-12">
                     English
                 </a>
             </li>
             <li>
                 <a href="{{ route('set.language', 'id') }}">
-                    <img src="images/flag_indonesia.png" alt="" class="w-auto">
+                    <img src="images/flag_indonesia.png" alt="" class="hidden md:block w-12">
                     Indonesia
                 </a>
             </li>
