03502fe99d8f969cd8fd20b2659a29a846436b91
yogis <yogisyo16@gmail.com>
2026-01-04 13:45:33 +0700

    Update: Fix npm issues - DaisyUI Not on package - Now can use the run build

--- resources/views/pages/responden/responden-act-question.blade.php
+++ resources/views/pages/responden/responden-act-question.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     <div class="flex flex-col justify-center items-center gap-6 max-w-auto transition-opacity opacity-100 duration-750 starting:opacity-0 md:items-center md:gap-5">
         <div class="flex flex-col items-center gap-2 md:items-center">
-            <ul class="font-arvo mt-4 mx-6 md:mx-32">
+            <ul class="font-arvo mt-4 mx-6 md:mx-44">
                 <li class="list-disc list-outside text-xl py-4">{{ __('messages.essay_question_1') }}</li>
                 <li class="list-disc list-outside text-xl py-4">{{ __('messages.essay_question_2') }}</li>
                 <li class="list-disc list-outside text-xl py-4">{{ __('messages.essay_question_3') }}</li>
